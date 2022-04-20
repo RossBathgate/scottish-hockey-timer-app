5bc75eababfd9bb136daf432a9dff4a427f8dd8d
Ross Bathgate <bathgate-r@outlook.com>
2022-04-20 11:31:12 +0100

    create GameScreen Boilerplate

---
+++ scottish-hockey-app/screens/GameScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const GameScreen = (props) => {
+    return <View style={styles.screen}></View>;
+};
+
+const styles = StyleSheet.create({
+    screen: { flex: 1 },
+});
+
+export default GameScreen;
