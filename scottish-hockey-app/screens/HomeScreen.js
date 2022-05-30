7dffa338627ee1d81c4a93864b298afbe10401dd
Ross Bathgate <bathgate-r@outlook.com>
2022-05-30 13:11:40 +0100

    Add new logo.  (Improve issue #11)

--- scottish-hockey-app/screens/HomeScreen.js
+++ scottish-hockey-app/screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
 import Menu from "../components/Home/Menu";
+import StopwatchSVG from "../assets/stopwatchLogo.svg";
 
 const HomeScreen = (props) => {
     return (
         <View style={styles.homeScreen}>
+            {/* <StopwatchSVG /> */}
             <Image
                 style={styles.stopwatchImage}
-                source={require("../assets/stopwatchLogo.png")}
+                source={require("../assets/stopwatchLogo2.png")}
             />
             <Menu
                 onPageChange={props.onPageChange}
@@ -19,7 +21,7 @@ const HomeScreen = (props) => {
 
 const styles = StyleSheet.create({
     homeScreen: { flex: 1, justifyContent: "space-between" },
-    stopwatchImage: { maxWidth: 700 },
+    stopwatchImage: { maxWidth: 700, top: -130, left: -100 },
 });
 
 export default HomeScreen;
